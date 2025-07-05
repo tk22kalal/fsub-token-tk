2310f1aea330d5d89ab7ed4e3036dfba4325f401
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-07-05 18:33:47 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -42,13 +42,13 @@ instance = Instance.from_db(db)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
+    file_id = fields.StrField(attribute='_id', required=True)
+    file_ref = fields.StrField(allow_none=True, default=None)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
+    file_type = fields.StrField(allow_none=True, default=None)
+    mime_type = fields.StrField(allow_none=True, default=None)
+    caption = fields.StrField(allow_none=True, default=None)
 
     class Meta:
         indexes = ('$file_name', )
