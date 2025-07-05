88e043232d5f249043f28056275379119846883c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-07-05 17:12:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -73,7 +73,6 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 
 #Database MONGO
-MONGO_URL = os.environ.get("MONGO_URL", "")
 MONGO_URI = os.environ.get("MONGO_URI", "")
 MONGO_NAME = os.environ.get("MONGO_NAME", "")
 
